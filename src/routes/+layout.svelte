d1a8125bf0c81d9323b6c945b1dfe9cf50188f6f
j12y <jayson.delancey@gmail.com>
2023-10-08 14:24:17 -0700

    tailwind with basic styling and overview

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="container mx-auto px-8 mt-6">
+    <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<svelte:head>
+    <title>Death by LLM</title>
+</svelte:head>
+
+<h1>Death <span class="text-4xl">b</span>y <span class="text-4xl">LLM</span></h1>
+<p>Prepare for the unexpected, create your e-will today.</p>
+<p>Have a happy and safe Halloween.</p>
+
+<h2>Generate a Will</h2>
+<p>Provide prompts to our artifici-will intelligent tool and generate a custom document to reflect your wishes.</p>
+<button>Create Will</button>
+
+<h2>Schedule Remote Witnesses</h2>
+<p>Invite your witnesses to view the real-time signing and attest to the authenticity with e-signatures.</p>
+<button>Begin Remote Witness Session</button>
+<button>Send for Signatures</button>
+
+
+<footer class="absolute bottom-0 inset-x-0 h-16 text-center">
+    <hr class="w-72 h-px mx-auto bg-gray-400 border-0 rounded dark:bg-gray-700 mb-3"/>
+    <p><i>This is a demo application and does not substitute for professional legal advice. Please consult with an attorney.</i></p>
+</footer>
