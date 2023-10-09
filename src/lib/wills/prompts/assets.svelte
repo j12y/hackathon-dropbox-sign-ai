8b1a5a215218ea764ac9bcb38964f6af9a45e75e
j12y <jayson.delancey@gmail.com>
2023-10-08 21:57:41 -0700

    user flow for will generator

---
+++ src/lib/wills/prompts/assets.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+ let name = '';
+ let residence = '';
+ let email = '';
+
+ export function getWillContent() {
+    return `
+        ARTICLE
+        PAYMENT OF DEBTS AND EXPENSES
+
+        I direct that my just debts, funeral expenses and expenses of last
+        illness be first paid from my estate.
+
+        ARTICLE
+        DISPOSITION OF PROPERTY
+
+    `
+ }
+
+</script>
+
+<div>
+    <label for="possession-1">Enter one of your most valuable possessions:</label>
+    <input id="possession-1" class="w-80" bind:value={ name } type="text">
+
+    <p><a class="text-blue-500" href="/not-implemented">Add Another</a></p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,82 @@
+<script lang="ts">
+    import WillGenerator from "$lib/wills/generator.svelte";
+
+
+    let isWillComplete = false;
+
+    let generator;
+    let isWillGeneratorActive = false;
+    let generatorButtonText = 'Get Started';
+
+    function handleWillClick(evt) {
+        isWillGeneratorActive = generator.toggle();
+        generatorButtonText = 'Edit Will';
+    }
+
+    function handleRemoteClick(evt) {
+        generator.hide();
+    }
+
+    function handleSignaturesClick(evt) {
+        generator.hide();
+    }
+
+</script>
 
 <svelte:head>
     <title>Death by LLM</title>
 </svelte:head>
 
-<h1>Death <span class="text-4xl">b</span>y <span class="text-4xl">LLM</span></h1>
-<p>Prepare for the unexpected, create your e-will today.</p>
-<p>Have a happy and safe Halloween.</p>
+<!-- Copyright 2023: Jayson DeLancey -->
 
-<h2>Generate a Will</h2>
-<p>Provide prompts to our artifici-will intelligent tool and generate a custom document to reflect your wishes.</p>
-<button>Create Will</button>
+<div class="flex flex-col h-screen">
+    <div class="flex items-center justify-center">
+        <div class="flex-row">
+            <h1>Death <span class="text-4xl">b</span>y <span class="text-4xl">LLM</span></h1>
+        </div>
+    </div>
+    <div class="flex items-center justify-center">
+        <p>Prepare for the unexpected, create an e-will for your loved ones today.</p>
+    </div>
+    <div class="flex items-center justify-center">
+        <p>Have a happy and safe Halloween.</p>
+    </div>
 
-<h2>Schedule Remote Witnesses</h2>
-<p>Invite your witnesses to view the real-time signing and attest to the authenticity with e-signatures.</p>
-<button>Begin Remote Witness Session</button>
-<button>Send for Signatures</button>
+    <div class="grid grid-cols-2 gap-4 mt-8">
+        <div>
+            <h2>Generate a Will</h2>
+            <p>Our artifici-will intelligent tool helps generate a custom document to reflect your wishes.</p>
+            <div class="object-center">
+                <button on:click={ handleWillClick }>
+                {#if isWillGeneratorActive }
+                Close
+                {:else}
+                    {generatorButtonText}
+                {/if}
+                </button>
+                {#if isWillComplete}
+                ✅ Will is prepared
+                {/if}
+            </div>
+        </div>
 
+        <div>
+            <h2>Schedule Remote Witnesses</h2>
+            <p>Invite two witnesses to view the real-time signing that can attest to the authenticity with e-signatures.</p>
+            <div class="justify-items-center">
+                <button on:click={ handleRemoteClick } disabled={!isWillComplete}>Begin Remote Witness Session</button>
+                <button on:click={ handleSignaturesClick } disabled={!isWillComplete}>Send for Signatures</button>
+            </div>
+        </div>
+    </div>
 
-<footer class="absolute bottom-0 inset-x-0 h-16 text-center">
-    <hr class="w-72 h-px mx-auto bg-gray-400 border-0 rounded dark:bg-gray-700 mb-3"/>
-    <p><i>This is a demo application and does not substitute for professional legal advice. Please consult with an attorney.</i></p>
-</footer>
+    <div class="flex h-4/6 w-screen">
+        <WillGenerator bind:this={ generator } bind:isWillComplete bind:isWillGeneratorActive/>
+    </div>
+
+    <footer class="absolute bottom-0 inset-x-0 h-16 text-center">
+        <hr class="w-72 h-px mx-auto bg-gray-400 border-0 rounded dark:bg-gray-700 mb-3"/>
+        <p><i>This is a demo application and does not substitute for professional legal advice. Please consult with an attorney.</i></p>
+    </footer>
+
+</div>
